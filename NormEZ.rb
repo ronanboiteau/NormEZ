7294669c3414ede051f848cde59c340cc8cd1333
Ronan Boiteau <ronan.boiteau@epitech.eu>
2017-12-18 17:22:14 +0100

    Improved README + Added colors to output

--- NormEZ.rb
+++ NormEZ.rb
@@ -1,3 +1,55 @@
+class String
+
+  def add_style(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
+
+  def black
+    add_style(31)
+  end
+
+  def red
+    add_style(31)
+  end
+
+  def green
+    add_style(32)
+  end
+
+  def yellow
+    add_style(33)
+  end
+
+  def blue
+    add_style(34)
+  end
+
+  def magenta
+    add_style(35)
+  end
+
+  def cyan
+    add_style(36)
+  end
+
+  def grey
+    add_style(37)
+  end
+
+  def bold
+    add_style(1)
+  end
+
+  def italic
+    add_style(3)
+  end
+
+  def underline
+    add_style(4)
+  end
+
+end
+
 class FileManager
 
   attr_accessor :path
@@ -50,7 +102,9 @@ class CodingStyleChecker
     line_nb = 1
     @file.each_line do |line|
       if line.length - 1 > 80
-        puts "[" + @file_path + ":" + line_nb.to_s + "] Too many columns (" + (line.length - 1).to_s + " > 80)"
+        msg_brackets = "[" + @file_path + ":" + line_nb.to_s + "]"
+        msg_error = " Too many columns (" + (line.length - 1).to_s + " > 80)"
+        puts msg_brackets.bold.magenta + msg_error.bold
       end
       line_nb += 1
     end
--- README.md
+++ README.md
@@ -2,6 +2,27 @@
 
 Coding-style checker for Epitech students.
 
+## Requirements
+
+ - [Ruby](https://www.ruby-lang.org/en/)
+
+### Installing Ruby on Fedora (Epitech's dump 2017)
+
+```
+sudo dnf install ruby
+```
+
+## How to use NormEZ?
+
+ - Clone the repository:
+```
+git clone https://github.com/ronanboiteau/NormEZ
+```
+ - Copy the `NormEZ.rb` executable 
+```
+git clone https://github.com/ronanboiteau/NormEZ
+```
+
 ## Features
 
 NormEZ check your `.c` and `.h` files for:
